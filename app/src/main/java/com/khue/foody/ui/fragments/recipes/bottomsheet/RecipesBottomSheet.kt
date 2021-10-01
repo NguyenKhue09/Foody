242d2ff352b35b6efe4d7a00482e9ac6d85a42b3
Khuê Nguyễn <19521712@ms.uit.edu.vn>
2021-10-01 07:27:27 +0700

    commpleted bottom sheet

--- app/src/main/java/com/khue/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
+++ app/src/main/java/com/khue/foody/ui/fragments/recipes/bottomsheet/RecipesBottomSheet.kt
@@ -1,23 +1,98 @@
 package com.khue.foody.ui.fragments.recipes.bottomsheet
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 import com.khue.foody.R
-
-
+import com.khue.foody.databinding.RecipesBottomSheetBinding
+import com.khue.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.khue.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.khue.foody.viewmodels.RecipesViewModel
+import java.util.*
 
 
 class RecipesBottomSheet : BottomSheetDialogFragment() {
 
+    private lateinit var recipesViewModel: RecipesViewModel
+    private var bottomSheetBinding: RecipesBottomSheetBinding? = null
+
+    private var mealTypeChip = DEFAULT_MEAL_TYPE
+    private var mealTypeChipId = 0
+    private var dietTypeChip = DEFAULT_DIET_TYPE
+    private var dietTypeChipId = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
-        return inflater.inflate(R.layout.recipes_bottom_sheet, container, false)
+        bottomSheetBinding = RecipesBottomSheetBinding.inflate(layoutInflater, container, false)
+
+        recipesViewModel.readMealAndDietType.asLiveData().observe(
+            viewLifecycleOwner,
+            {
+                value ->
+                mealTypeChip = value.selectedMealType
+                dietTypeChip = value.selectedMealType
+                updateChip(value.selectedMealTypeId, bottomSheetBinding!!.mealTypeChipGroup)
+                updateChip(value.selectedDietTypeId, bottomSheetBinding!!.dietTypeChipGroup)
+            }
+        )
+
+        bottomSheetBinding!!.mealTypeChipGroup.setOnCheckedChangeListener{
+            group, selectedChipId ->
+            val chip = group.findViewById<Chip>(selectedChipId)
+            val selectedMealType = chip.text.toString().lowercase(Locale.ROOT)
+            mealTypeChip = selectedMealType
+            mealTypeChipId = selectedChipId
+        }
+
+        bottomSheetBinding!!.dietTypeChipGroup.setOnCheckedChangeListener {
+            group, checkedId ->
+            val chip = group.findViewById<Chip>(checkedId)
+            val selectedDietType = chip.text.toString().lowercase(Locale.ROOT)
+            dietTypeChip = selectedDietType
+            dietTypeChipId = checkedId
+        }
+
+        bottomSheetBinding!!.applyBtn.setOnClickListener {
+            recipesViewModel.saveMealAndDietType(
+                mealTypeChip,
+                mealTypeChipId,
+                dietTypeChip,
+                dietTypeChipId
+            )
+            val action =
+                RecipesBottomSheetDirections.actionRecipesBottomSheetToRecipesFragment(true)
+            findNavController().navigate(action)
+        }
+
+        return bottomSheetBinding!!.root
+    }
+
+    private fun updateChip(chipId: Int, chipGroup: ChipGroup) {
+        if (chipId != 0) {
+            try {
+               chipGroup.findViewById<Chip>(chipId).isChecked = true
+            } catch (e: Exception) {
+                Log.d("RecipesBottomSheet", e.message.toString())
+            }
+        }
     }
 
 
--- app/src/main/java/com/khue/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/khue/foody/viewmodels/RecipesViewModel.kt
@@ -2,18 +2,50 @@ package com.khue.foody.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.khue.foody.data.DataStoreRepository
+import com.khue.foody.data.MealAnDietType
 import com.khue.foody.util.Constants
+import com.khue.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.khue.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.khue.foody.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application) : AndroidViewModel(application){
+@HiltViewModel
+class RecipesViewModel @Inject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application){
 
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) =
+        viewModelScope.launch(Dispatchers.IO) {
+            dataStoreRepository.saveMealAnDietType(mealType, mealTypeId, dietType, dietTypeId)
+        }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
-        queries[Constants.QUERY_NUMBER] = "50"
+        viewModelScope.launch {
+            readMealAndDietType.collect { value ->  
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
+        queries[Constants.QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[Constants.QUERY_API_KEY] = Constants.API_KEY
-        queries[Constants.QUERY_TYPE] = "snack"
-        queries[Constants.QUERY_DIET] = "vegan"
+        queries[Constants.QUERY_TYPE] = mealType
+        queries[Constants.QUERY_DIET] = dietType
         queries[Constants.QUERY_ADD_RECIPES_INFORMATION] = "true"
         queries[Constants.QUERY_FILL_INGREDIENTS] = "true"
         queries[Constants.QUERY_ADD_RECIPE_NUTRITION] = "true"
